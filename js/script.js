1562644a20b60c108e33e84608c3d609d84c806e
GiadaGallitto <gallitto.giada@gmail.com>
2022-11-22 14:50:24 +0100

    Create input base structure and Add Js const

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+// SCHEMA ESERCIZIO
+
+// MileStone 1 (Creazione richieste base e calcoli):
+
+// -Chiedo (Senza prompt) il numero di chilometri da percorrere:
+
+//     Creo un imput type number per inserire il numero desiderato dall'utente;
+//         -Definisco il prezzo base di 0.2762 euro al km
+//         -Calcolo il prezzo base del biglietto, moltiplicando il prezzo di 0.2762 euro per i km inseriti
+
+// -Chiedo (Senza prompt) l'età del passeggero:
+
+//     Creo un input type number per inserire il numero di anni dell'utente;
+//         -Applico un possibile sconto in base all'età del passeggero:
+//             -Se il passeggero ha meno di 18 anni, allora verrà applicato uno sconto del 17.5% sul prezzo base del biglietto
+//                 Altrimenti non verrà applicato lo sconto
+            
+//             -Se il passeggero ha un'età >= di 65 anni, allora verrà applicato uno sconto del 33.3% sul prezzo base del biglietto
+//                 Altrimenti non verrà applicato lo sconto
+
+// -Creo un button da cliccare per ottenere il risultato del prezzo totale:
+
+
+const priceForKm = 0.2762;
+
+console.log("Il prezzo base al Km è di: " + priceForKm + " €" );
+
+const inputUserKm = document.querySelector("input#userKm");
+
+const output = document.getElementById("output");
+
+const button = document.querySelector("div.buttons a.btn");
+
